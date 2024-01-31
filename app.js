60951bb2a7cd00c1ee1d6c5d777eab1bb4303a61
AdrianScharf <adrian.jrez99@gmail.com>
2024-01-31 11:36:14 -0600

    Upload Live-Counter project

---
+++ app.js
@@ -0,0 +1,48 @@
+const getCurrentStreak = (units, startDate) => {
+    const startTime = startDate.getTime();
+    const currentTime = new Date().getTime();
+
+    const currentStreak = currentTime - startTime;
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMin = 60 * 1000;
+    const oneSec = 1000;
+
+    let days = Math.floor(currentStreak / oneDay);
+    let hours = Math.floor((currentStreak % oneDay) / oneHour);
+    let minutes = Math.floor((currentStreak % oneHour) / oneMin);
+    let seconds = Math.floor((currentStreak % oneMin) / oneSec);
+
+    const values = [days, hours, minutes, seconds];
+
+    const setValue = (value) => {
+        if (value < 10) {
+            return `0${value}`
+        }
+        return value;
+    }
+
+    units.forEach((item, index) => {
+        item.innerHTML = setValue(values[index]);
+    });
+}
+
+
+// NoFap
+const itemsNf = document.querySelectorAll('p .num-nf');
+console.log(itemsNf);
+const startDateNf = new Date(2024, 0, 31, 5, 36, 0);
+
+setInterval(() => {
+    getCurrentStreak(itemsNf, startDateNf)
+}, 1000);
+
+
+// Semen Retention
+const itemsSr = document.querySelectorAll('p .num-sr');
+const startDateSr = new Date(2024, 0, 31, 5, 36, 0);
+
+setInterval(() => {
+    getCurrentStreak(itemsSr, startDateSr)
+}, 1000);
